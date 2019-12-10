f29b5d94283fcf81659cb9a431c36e6de7447baf
Nicklas Kamp Christiansen <nick7896@edu.easj.dk>
2019-12-10 12:32:12 +0100

    Added Booking.cs file

---
+++ VisualBooking/Booking.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualBooking
+{
+    class Booking
+    {
+        public DateTime Date { get; set; }
+        public DateTime StartTime { get; set; }
+        public string PhoneNr { get; set; }
+        public string Name { get; set; }
+        public int Length { get; set; }
+        public int Patrons { get; set; }
+
+        public Booking(DateTime date, DateTime startTime, string phoneNr, string name, int patrons) {
+            Date = date;
+            StartTime = startTime;
+            PhoneNr = phoneNr;
+            Name = name;
+            Patrons = patrons;
+        }
+
+        public override string ToString() {
+            return $"Date: {Date}, StartTime: {StartTime}, PhoneNr: {PhoneNr}, Name: {Name}, Patrons: {Patrons}";
+        }
+    }
+}
