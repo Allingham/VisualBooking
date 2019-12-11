482d0cfe43885e4a9359d68fe55b41b1c188330c
Alexander Grove Lundin <alex135r@edu.easj.dk>
2019-12-11 15:29:45 +0100

    add relay

--- VisualBooking/ViewModel.cs
+++ VisualBooking/ViewModel.cs
@@ -24,7 +24,7 @@ namespace VisualBooking
 
         public ViewModel()
         {
-            AddCommand = new RelayCommand(Add());
+            AddCommand = new RelayCommand(Add);
             SaveCommand = new RelayCommand(Save);
         }
 
