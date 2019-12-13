2216c5443c513f107fd2423bda1c0525c541e8a4
Christian Allingham <chri793r@edu.easj.dk>
2019-12-13 12:16:00 +0100

    funktionalitet

--- VisualBooking/Booking.cs
+++ VisualBooking/Booking.cs
@@ -8,23 +8,20 @@ namespace VisualBooking
 {
     class Booking
     {
-        public DateTime Date { get; set; }
-        public DateTime StartTime { get; set; }
+        public DateTimeOffset Date { get; set; }
         public string PhoneNr { get; set; }
         public string Name { get; set; }
-        public int Length { get; set; }
         public int Patrons { get; set; }
 
-        public Booking(DateTime date, DateTime startTime, string phoneNr, string name, int patrons) {
+        public Booking(DateTimeOffset date, string phoneNr, string name, int patrons) {
             Date = date;
-            StartTime = startTime;
             PhoneNr = phoneNr;
             Name = name;
             Patrons = patrons;
         }
 
         public override string ToString() {
-            return $"Date: {Date}, StartTime: {StartTime}, PhoneNr: {PhoneNr}, Name: {Name}, Patrons: {Patrons}";
+            return $"Date: {Date}, PhoneNr: {PhoneNr}, Name: {Name}, Patrons: {Patrons}";
         }
     }
 }
